b4872328577fc2cd8ecf171856221be425d9d067
ABSO <atargaming901@gmail.com>
2026-02-10 23:19:10 +0300

    Update mods list

---
+++ api/mods.php
@@ -0,0 +1,17 @@
+<?php
+// ملف المودات
+$mods = array (
+  'mod_698b92bab41d80.14682125' => 
+  array (
+    'downloads' => 0,
+    'id' => 'mod_698b92bab41d80.14682125',
+    'image' => 'https://testwebmuff.page.gd/images/mod_698b92bab41d80.14682125.jpg',
+    'link' => 'https://testwebmuff.page.gd/files/mod_698b92bab41d80.14682125.json',
+    'name' => 'g',
+    'number' => 1,
+    'timestamp' => 1770754748000,
+    'version' => '1.0',
+    'views' => 0,
+  ),
+);
+?>
